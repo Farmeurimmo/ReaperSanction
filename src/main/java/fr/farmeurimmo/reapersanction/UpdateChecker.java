e90fd41b339a4d50d41dc952facd27cf3eca1c19
Farmeurimmo <farmeurimmo@gmail.com>
2023-09-30 13:50:57 +0200

    Better UpdateChecker, updated maven

--- src/main/java/fr/farmeurimmo/reapersanction/UpdateChecker.java
+++ src/main/java/fr/farmeurimmo/reapersanction/UpdateChecker.java
@@ -1,9 +1,13 @@
 package fr.farmeurimmo.reapersanction;
 
 import fr.farmeurimmo.reapersanction.api.Main;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.function.Consumer;
@@ -26,27 +30,78 @@ public class UpdateChecker {
         }
     }
 
+    public JSONObject getVersionViaAPI() {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL("https://api.farmeurimmo.fr/plugins/89580").openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream responseStream = connection.getInputStream();
+            Scanner scanner = new Scanner(responseStream);
+
+            String response = scanner.useDelimiter("\\A").next();
+            scanner.close();
+            responseStream.close();
+
+            return (JSONObject) new JSONParser().parse(response);
+
+        } catch (IOException | ParseException ignored) {
+        }
+        return new JSONObject();
+    }
+
     public void checkForUpdate(String v, Main console) {
         if (v.contains("ERROR")) {
             console.sendLogMessage("§c§lCan't find plugin version to check for update, please check for update manually !", 2);
             return;
         }
+
         new UpdateChecker(89580).getVersion(version -> {
-            if (v.equals(version)) {
-                console.sendLogMessage("§6No update found.", 0);
-            } else {
-                if (version.contains("RC")) {
-                    console.sendLogMessage("§c§lA new Release candidate update is available, you can try it but it may contains bugs and " +
-                            "breaking changes so be careful !", 0);
-                } else {
-                    console.sendLogMessage("A new update is available please consider updating if you want to receive support !", 1);
-                    console.sendLogMessage("§cNewest version detected at spigot : §4§l" + version, 1);
-                    console.sendLogMessage("§6Your version : §c" + v, 1);
-                    console.sendLogMessage("§6Download link : §ahttps://reaper.farmeurimmo.fr/reapersanction/", 1);
-                    console.sendLogMessage("§4§lA new update is available please consider updating if you want to receive support !" +
-                            " (the spigot api is taking time to update the version)", 1);
+            JSONObject json = getVersionViaAPI();
+            if (json != null && json.containsKey("version") && !json.get("version").equals(version)) {
+                if (isLatest(json.get("version").toString(), version)) {
+                    version = json.get("version").toString();
+                    return;
                 }
+                return;
             }
+            boolean latest = isLatest(v, version);
+            console.sendLogMessage("§6Detected version : §b" + v + "§6, Spigot version : §b" + version, 0);
+            if (version.contains("RC")) {
+                if (latest) {
+                    console.sendLogMessage("§6No update found. §eYou are using a release candidate version, bugs may be present ! §b" + v, 0);
+                    return;
+                }
+                console.sendLogMessage("§c§lA Release Candidate version is available, you are currently using a stable version, " +
+                        "you can try the new version but bugs may be present !", 2);
+                return;
+            }
+            if (!latest) {
+                console.sendLogMessage("A new update is available please consider updating if you want to receive support !", 1);
+                console.sendLogMessage("§cNewest version detected at spigot : §4§l" + version, 1);
+                console.sendLogMessage("§6Your version : §c" + v, 1);
+                console.sendLogMessage("§6Download link : §ahttps://reaper.farmeurimmo.fr/reapersanction/", 1);
+                console.sendLogMessage("§4§lA new update is available please consider updating if you want to receive support !" +
+                        " (the spigot api is taking time to update the version)", 1);
+                return;
+            }
+            console.sendLogMessage("§6No update found.", 0);
+
         });
+    }
+
+    public boolean isLatest(String currentVersion, String target) {
+        String[] current = currentVersion.split("\\.");
+        String[] targetVersion = target.split("\\.");
+
+        if (current[2].contains("-RC")) { //prevents RC+(number) to be make error while checking for update
+            current[2] = current[2].split("-RC")[0];
+        }
+
+        for (int i = 0; i < current.length; i++) {
+            int c = Integer.parseInt(current[i]);
+            int t = Integer.parseInt(targetVersion[i]);
+            if (c < t) return false;
+        }
+        return true;
     }
 }
